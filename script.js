f02cf132e91f9e8703f29bb74c3af9d656c2544b
BoomSlang <boom.slang.r7@gmail.com>
2022-08-18 22:23:03 +0700

    edit the js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const dateElement = document.getElementById("date");
 let addToDo = document.getElementById('addToDo');
 let toDoPage = document.getElementById('toDoPage');
 let inputField = document.getElementById('inputField');
+
+clear.addEventListener("click", function(){
+    location.reload();
+});
