e780e0a9571271d5240c0f6efbb0e7a9fe73f71a
BoomSlang <boom.slang.r7@gmail.com>
2022-08-18 22:22:10 +0700

    get the id

---
+++ script.js
@@ -0,0 +1,5 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+let addToDo = document.getElementById('addToDo');
+let toDoPage = document.getElementById('toDoPage');
+let inputField = document.getElementById('inputField');
